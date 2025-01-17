59a493798ada642b8b54f7c3cf9e73effd02684d
vaibdix <dixitvaib@gmail.com>
2025-01-17 12:02:58 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import AppContext from './components/context/AppContext'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './components/routes/AppRoute'
+import AppContext from './components/context/AppContext';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './components/routes/AppRoute';
 
 const App = () => {
   return (
     <AppContext>
       <RouterProvider router={router} />
     </AppContext>
+  );
+};
 
-  )
-}
+export default App;
 
-export default App
+// https://github.com/prahulk46a?tab=repositories
