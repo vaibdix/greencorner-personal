0c0817b6ac5f957b93db95f7267d9975bce543e1
vaibdix <dixitvaib@gmail.com>
2025-01-21 21:57:29 +0530

    chore: updated routes for outlet

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,30 +1,58 @@
+// import { useContext } from 'react';
+// import { context } from '../../context/AppContext';
+// import Hero from '../Hero/Hero';
+// import Header from '../Header/Header';
+// import { Outlet } from 'react-router-dom';
+
+// import Footer from '../Footer/Footer';
+// import NewsLetter from '../NewsLetter/NewsLetter';
+// import CateFilter from '../CateFilter/CateFilter';
+// import HomeCategory from '../HomeCategory/HomeCategory';
+// import CardFive from '../CardFive';
+// import CardTen from '../CardTen';
+
+// const Home = () => {
+//   const plants = useContext(context);
+//   console.log(plants);
+//   return (
+//     <div>
+//       <Header />
+//       <Hero />
+//       <CardFive />
+//       <HomeCategory />
+
+//       {/* <Outlet /> */}
+//       <CardTen />
+//       <NewsLetter />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
+// src/pages/Home/Home.jsx
 import { useContext } from 'react';
 import { context } from '../../context/AppContext';
 import Hero from '../Hero/Hero';
-import Header from '../Header/Header';
-import { Outlet } from 'react-router-dom';
-
-import Footer from '../Footer/Footer';
-import NewsLetter from '../NewsLetter/NewsLetter';
-import CateFilter from '../CateFilter/CateFilter';
 import HomeCategory from '../HomeCategory/HomeCategory';
 import CardFive from '../CardFive';
 import CardTen from '../CardTen';
+import NewsLetter from '../NewsLetter/NewsLetter';
 
 const Home = () => {
   const plants = useContext(context);
   console.log(plants);
+
   return (
     <div>
-      <Header />
       <Hero />
       <CardFive />
       <HomeCategory />
-
-      {/* <Outlet /> */}
       <CardTen />
       <NewsLetter />
-      <Footer />
     </div>
   );
 };
--- src/components/routes/AppRoute.jsx
+++ src/components/routes/AppRoute.jsx
@@ -1,30 +1,77 @@
+// import { createBrowserRouter } from 'react-router-dom';
+// import Home from '../pages/Home/Home';
+// import SignIn from '../pages/Auth/SignIn';
+// import SignUp from '../pages/Auth/SignUp';
+// import CateFilter from '../pages/CateFilter/CateFilter';
+// import Product from '../pages/Product/Product';
+
+// export const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home />,
+//   },
+//   {
+//     path: '/signin',
+//     element: <SignIn />,
+//   },
+//   {
+//     path: '/signup',
+//     element: <SignUp />,
+//   },
+//   {
+//     path: '/category',
+//     element: <CateFilter />,
+//   },
+//   {
+//     path: '/product',
+//     element: <Product />
+//   }
+
+// ]);
+
+
+
+// src/routes/AppRoute.jsx
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import SignIn from '../pages/Auth/SignIn';
 import SignUp from '../pages/Auth/SignUp';
 import CateFilter from '../pages/CateFilter/CateFilter';
 import Product from '../pages/Product/Product';
+import MainLayout from '../../layouts/MainLayout'; // Import MainLayout
+import NotFound from '../pages/NotFound/NotFound';
 
 export const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <MainLayout />, // Use MainLayout as the wrapper
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/category',
+        element: <CateFilter />,
+      },
+      {
+        path: '/product',
+        element: <Product />,
+      },
+      {
+        path: '/signin',
+        element: <SignIn />,
+      },
+      {
+        path: '/signup',
+        element: <SignUp />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
   },
-  {
-    path: '/signin',
-    element: <SignIn />,
-  },
-  {
-    path: '/signup',
-    element: <SignUp />,
-  },
-  {
-    path: '/category',
-    element: <CateFilter />,
-  },
-  {
-    path: '/product',
-    element: <Product />
-  }
+  
 
 ]);
