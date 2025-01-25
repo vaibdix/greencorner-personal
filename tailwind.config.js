c24f39ad32d29afb771b0d68c3d25a7ab29dfdea
vaibdix <dixitvaib@gmail.com>
2025-01-25 20:30:36 +0530

    chore: img added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        sage: {
+          50: '#f8faf8',
+          100: '#f0f4f1',
+          200: '#dce3dd',
+          600: '#637b66',
+        },
+      },
+    },
   },
   plugins: [],
 };
