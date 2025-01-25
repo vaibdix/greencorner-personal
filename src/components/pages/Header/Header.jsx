4cea9e5933fd383ea93fd08fb0840c5244000c68
vaibdix <dixitvaib@gmail.com>
2025-01-26 02:53:03 +0530

    chore: updated wishlist counter

--- src/components/pages/Header/Header.jsx
+++ src/components/pages/Header/Header.jsx
@@ -9,6 +9,8 @@ const Header = () => {
 
   // Calculate total items in cart
   const cartItemsCount = state.cart.reduce((total, item) => total + item.quantity, 0);
+  // Calculate total items in wishlist
+  const wishlistCount = state.wishlist.length;
 
   // Function to toggle mobile menu visibility
   const toggleMobileMenu = () => {
@@ -25,7 +27,7 @@ const Header = () => {
               <span className="self-center text-xl font-semibold">The Green Corner</span>
             </Link>
             <div className="flex items-center gap-3 lg:order-2">
-              <Link to="/" className="rounded-lg py-2 text-sm font-medium text-gray-800 lg:py-2.5">
+              <Link to="/wishlist" className="relative rounded-lg py-2 text-sm font-medium text-gray-800 lg:py-2.5">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
@@ -40,6 +42,11 @@ const Header = () => {
                 >
                   <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
                 </svg>
+                {wishlistCount > 0 && (
+                  <span className="absolute -right-2 -top-0 flex h-5 w-5 items-center justify-center rounded-full bg-red-500 text-xs text-white">
+                    {wishlistCount}
+                  </span>
+                )}
               </Link>
               <Link
                 to="/signin"
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,54 +1,13 @@
-// import { useContext } from 'react';
-// import { context } from '../../context/AppContext';
-// import Hero from '../Hero/Hero';
-// import Header from '../Header/Header';
-// import { Outlet } from 'react-router-dom';
-
-// import Footer from '../Footer/Footer';
-// import NewsLetter from '../NewsLetter/NewsLetter';
-// import CateFilter from '../CateFilter/CateFilter';
-// import HomeCategory from '../HomeCategory/HomeCategory';
-// import CardFive from '../CardFive';
-// import CardTen from '../CardTen';
-
-// const Home = () => {
-//   const plants = useContext(context);
-//   console.log(plants);
-//   return (
-//     <div>
-//       <Header />
-//       <Hero />
-//       <CardFive />
-//       <HomeCategory />
-
-//       {/* <Outlet /> */}
-//       <CardTen />
-//       <NewsLetter />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
 
 // src/pages/Home/Home.jsx
-import { useContext } from 'react';
-import { context } from '../../context/AppContext';
 import Hero from '../Hero/Hero';
-import HomeCategory from '../HomeCategory/HomeCategory';
 import CardFive from '../CardFive';
 import CardTen from '../CardTen';
 import NewsLetter from '../NewsLetter/NewsLetter';
 import CouponAdvertise from '../CouponAdvertise/CouponAdvertise';
 
 const Home = () => {
-  const plants = useContext(context);
-  console.log(plants);
-
-
-
+ 
   return (
     <div>
       <Hero />
