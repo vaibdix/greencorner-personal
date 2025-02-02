e4ecbd23a87ed510e27b9c3297cc77fbf8a2c8e2
vaibdix <dixitvaib@gmail.com>
2025-02-02 14:16:38 +0530

    fix: ui

--- src/features/review/Review.jsx
+++ src/features/review/Review.jsx
@@ -21,14 +21,14 @@ const Review = ({ reviews }) => {
         {reviewsToDisplay.length > 0 ? (
           reviewsToDisplay.map((review, index) => (
             <div key={index} className="pt-8 max-xl:mx-auto">
-              <div className="mb-4 flex flex-col justify-between gap-5 min-[400px]:flex-row sm:items-center">
+              <div className="mb-2 flex flex-col justify-between gap-5 min-[400px]:flex-row sm:items-center">
                 <div className="flex items-center gap-3">
                   <img
                     src={review.userAvatar || 'https://pagedone.io/asset/uploads/1704351103.png'} // Default if no avatar
                     alt={`${review.username} image`}
                     className="h-8 w-8 rounded-full object-cover"
                   />
-                  <h6 className="text-lg leading-8 font-semibold text-indigo-600">
+                  <h6 className="text-md leading-8 font-semibold text-[#1C3035]">
                     {review.username}
                   </h6>
                 </div>
@@ -36,27 +36,29 @@ const Review = ({ reviews }) => {
               </div>
 
               {/* Rating Stars */}
-              <div className="mb-4 ml-10 flex items-center gap-3">
+              <div className="mb-1 ml-10 flex items-center gap-2">
                 {Array.from({ length: 5 }, (_, i) => (
                   <Star
                     key={i}
-                    className={`text-amber-400 ${i < review.rating ? 'fill-current' : 'text-gray-300'}`}
+                    size={14}
+                    className={`${i < review.rating ? 'text-amber-500' : 'text-gray-300'}`}
+                    strokeWidth={2}
                   />
                 ))}
               </div>
 
               {/* Review Text */}
-              <p className="ml-10 text-lg leading-8 font-normal text-gray-400 max-xl:text-justify">
+              <p className="text-md ml-10 leading-8 font-normal text-gray-400 max-xl:text-justify">
                 {review.comment}
               </p>
 
               {/* Likes and Dislikes */}
-              <div className="mt-4 ml-10 flex items-center gap-6">
-                <button className="flex items-center text-green-600 hover:text-green-800">
-                  <span className="mt-1 pr-2 font-semibold">{review.likes}</span> <ThumbsUp />
+              <div className="mt-2 ml-10 flex items-center gap-6">
+                <button className="flex items-center text-[#1C3035] hover:text-green-800">
+                  <span className="pr-2">{review.likes}</span> <ThumbsUp size={16} />
                 </button>
                 <button className="flex items-center text-red-600 hover:text-red-800">
-                  <span className="mt-1 pr-2 font-semibold">{review.dislikes}</span> <ThumbsDown />
+                  <span className="pr-2">{review.dislikes}</span> <ThumbsDown size={16} />
                 </button>
               </div>
             </div>
@@ -69,7 +71,7 @@ const Review = ({ reviews }) => {
         {reviews.length > initialReviewCount && (
           <button
             onClick={() => setShowAllReviews(!showAllReviews)}
-            className="mt-6 font-semibold text-blue-600 hover:text-blue-800"
+            className="mt-4 text-blue-600 hover:text-blue-800"
           >
             {showAllReviews ? 'Show Less' : 'Show More'}
           </button>
